916ad6eb92d1bd52e4ce009572bb7b1e32014cc7
Hermann Krumrey <hermann@krumreyh.com>
2016-05-17 14:36:42 +0200

    Implemented setup.py

--- hikvision_client/metadata.py
+++ hikvision_client/metadata.py
@@ -19,3 +19,104 @@ This file is part of hikvision-client.
     You should have received a copy of the GNU General Public License
     along with hikvision-client.  If not, see <http://www.gnu.org/licenses/>.
 """
+
+"""
+The metadata is stored here. It can be used by any other module in this project this way, most
+notably by the setup.py file
+"""
+
+project_name = "hikvision_client"
+"""
+The name of the project
+"""
+
+project_description = "A graphical client software for Hikvision (and other RTSP-enabled) IP cameras"
+"""
+A short description of the project
+"""
+
+version_number = "0.1"
+"""
+The current version of the program.
+"""
+
+development_status = "Development Status :: 2 - Pre-Alpha"
+"""
+The current development status of the program
+"""
+
+audience = "Intended Audience :: End Users/Desktop"
+"""
+The intended audience of this software
+"""
+
+environment = "Environment :: Other Environment"
+"""
+The intended environment in which the program will be used
+"""
+
+programming_language = 'Programming Language :: Python :: 3'
+"""
+The programming language used in this project
+"""
+
+topic = "Topic :: Multimedia :: Video :: Display"
+"""
+The broad subject/topic of the project
+"""
+
+language = "Natural Language :: English"
+"""
+The (default) language of this project
+"""
+
+compatible_os = "Operating System :: OS Independent"
+"""
+Th eOperating Systems on which the program can run
+"""
+
+project_url = "http://namibsun.net/namboy94/hikvision-client"
+"""
+A URL linking to the home page of the project, in this case a
+self-hosted Gitlab page
+"""
+
+download_url = "http://gitlab.namibsun.net/namboy94/hikvision-client/repository/archive.zip?ref=master"
+"""
+A URL linking to the current source zip file.
+"""
+
+author_name = "Hermann Krumrey"
+"""
+The name(s) of the project author(s)
+"""
+
+author_email = "hermann@krumreyh.com"
+"""
+The email address(es) of the project author(s)
+"""
+
+license_type = "GNU GPL3"
+"""
+The project's license type
+"""
+
+license_identifier = "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
+"""
+The license used for this project
+"""
+
+pypi_requirements = []
+"""
+List of requirements available on pypi.
+"""
+
+other_requirements = []
+"""
+List of requirements not available on pypi
+"""
+
+scripts = []
+"""
+List of script files to be installed during installation
+"""
--- setup.py
+++ setup.py
@@ -19,3 +19,51 @@ This file is part of hikvision-client.
     You should have received a copy of the GNU General Public License
     along with hikvision-client.  If not, see <http://www.gnu.org/licenses/>.
 """
+
+# imports
+import hikvision_client.metadata as metadata
+from setuptools import setup, find_packages
+
+
+def readme() -> str:
+    """
+    Reads the readme file and converts it from markdown to restructured text
+
+    :return: the readme file as a string
+    """
+    try:
+        # noinspection PyPackageRequirements
+        import pypandoc
+        with open('README.md') as f:
+            # Convert markdown file to rst
+            markdown = f.read()
+            rst = pypandoc.convert(markdown, 'rst', format='md')
+            return rst
+    except (OSError, ImportError):
+        # If pandoc is not installed, just return the raw markdown text
+        with open('README.md') as f:
+            return f.read()
+
+setup(name=metadata.project_name,
+      version=metadata.version_number,
+      description=metadata.project_description,
+      long_description=readme(),
+      classifiers=[metadata.development_status,
+                   metadata.license_identifier,
+                   metadata.audience,
+                   metadata.programming_language,
+                   metadata.topic,
+                   metadata.language,
+                   metadata.compatible_os,
+                   metadata.environment],
+      url=metadata.project_url,
+      author=metadata.author_name,
+      author_email=metadata.author_email,
+      license=metadata.license_type,
+      packages=find_packages(),
+      install_requires=metadata.pypi_requirements,
+      requires=metadata.other_requirements,
+      scripts=metadata.scripts,
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      zip_safe=False)
